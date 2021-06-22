8cbede757ead7d101524d8953397f0304b148868
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-06-22 10:04:20 +0200

    05_Delete Student: Path Variable

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -47,8 +47,8 @@ public class StudentController {
 		return new StudentResponse(student);
 	}
 
-	@DeleteMapping("delete")
-	public String deleteStudent (@RequestParam long id) {
+	@DeleteMapping("delete/{id}")
+	public String deleteStudent (@PathVariable long id) {
 		return studentService.deleteStudent(id);
 	}
 }
